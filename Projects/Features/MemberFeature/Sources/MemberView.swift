bc17d797dd0941dc55df9865711bf381b9f4c279
indextrown <indextrown@gmail.com>
2026-03-04 17:16:26 +0900

    [#31] Member 모듈 생성

---
+++ Projects/Features/MemberFeature/Sources/MemberView.swift
@@ -0,0 +1,8 @@
+//
+//  MemberView.swift
+//  MemberFeature
+//
+//  Created by 김동현 on 3/4/26.
+//
+
+import Foundation
---
+++ Projects/Features/MemberFeature/Sources/MemberViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MemberViewModel.swift
+//  MemberFeature
+//
+//  Created by 김동현 on 3/4/26.
+//
+
+import Foundation
