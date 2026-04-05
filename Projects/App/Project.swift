1f8aa7f39555cd648ff3e195d7d9c605dbc685ff
indextrown <indextrown@gmail.com>
2026-04-05 18:33:18 +0900

    [#43] appstore 국가 설정

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -55,6 +55,10 @@ let releaseSettings: SettingsDictionary = [
 
 let project = Project(
     name: "App",
+    options: .options(
+        defaultKnownRegions: ["en", "ko"],
+        developmentRegion: "ko"
+    ),
     targets: [
         .target(
             name: "App",
