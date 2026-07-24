08965a3634e43e48626d0e98fec8dfcdde794450
indextrown <indextrown@gmail.com>
2026-07-24 14:10:14 +0900

    chore: 초기 컴포넌트 스케치 보관

--- Projects/Shared/CollectionViewAdapter/Sources/Empty.swift
+++ Projects/Shared/CollectionViewAdapter/Sources/Empty.swift
@@ -1,5 +1,53 @@
-
-
-protocol ComponentContext {
-    
-}
+//import UIKit
+//
+//struct ComponentContext {
+//    let indexPath: IndexPath
+//}
+//
+//protocol Component {
+//    associatedtype Content: UIView
+//    
+//    func createContent() -> Content
+//    func render(context: ComponentContext, content: Content)
+//    func size(in collectionView: UICollectionView) -> CGSize
+//}
+//
+//final class ContainerCell<C: Component>: UICollectionViewCell {
+//    private var hostedContent: C.Content?
+//    
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//    }
+//    
+//    @available(*, unavailable)
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//    func render(
+//        component: C,
+//        context: ComponentContext
+//    ) {
+//        let content: C.Content
+//        
+//        if let hostedContent {
+//            content = hostedContent
+//        } else {
+//            let newContent = component.createContent()
+//            newContent.translatesAutoresizingMaskIntoConstraints = false
+//            contentView.addSubview(newContent)
+//            
+//            NSLayoutConstraint.activate([
+//                newContent.topAnchor.constraint(equalTo: contentView.topAnchor),
+//                newContent.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+//                newContent.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+//                newContent.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+//            ])
+//            
+//            hostedContent = newContent
+//            content = newContent
+//        }
+//        component.render(context: context, content: content)
+//    }
+//}
+//
