be4e0e4c4fa7f4aa3ade97de4a3417480251da76
indextrown <indextrown@gmail.com>
2026-08-09 00:33:48 +0900

    chore: 앱 및 알림 공지 버전을 1.1.2로 변경

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -72,7 +72,7 @@ let project = Project(
                 // "CFBundleDisplayName": "하루한컷",
 
                 // 앱 버전 설정
-                "CFBundleShortVersionString": "1.1.1",
+                "CFBundleShortVersionString": "1.1.2",
 
                 // 빌드 번호 설정
                 "CFBundleVersion": "$(CURRENT_PROJECT_VERSION)",
