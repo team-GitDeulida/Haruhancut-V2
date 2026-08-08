6942a901992d9af0b14d42624fe922359ce08c6e
indextrown <indextrown@gmail.com>
2026-08-09 02:58:04 +0900

    chore: 다운샘플링 + 원본 로직 주석 추가

--- Projects/Features/HomeFeatureV2/Sources/Feed/FeedDetail/FeedDetailView.swift
+++ Projects/Features/HomeFeatureV2/Sources/Feed/FeedDetail/FeedDetailView.swift
@@ -79,8 +79,11 @@ final class FeedDetailView: UIView {
 
         imageView.kf.cancelDownloadTask()
         currentImageURL = imageURL
+        
+        // 1. 프로필 그리드에서 넘겨받은 이미지 즉시 표시
         imageView.image = initialImage
 
+        // 2. 같은 URL로 원본 이미지 요청
         imageView.kf.setImage(
             with: url,
             options: [
@@ -96,5 +99,6 @@ final class FeedDetailView: UIView {
             }
             self?.currentImageURL = nil
         }
+        
     }
 }
