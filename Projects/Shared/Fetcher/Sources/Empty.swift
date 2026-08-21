a450b32ed2e420d4476a56ab59338148ad944f7a
indextrown <indextrown@gmail.com>
2026-08-21 13:55:35 +0900

    build: add Fetcher module scaffold

---
+++ Projects/Shared/Fetcher/Sources/Empty.swift
@@ -0,0 +1,6 @@
+//
+//  Empty.swift
+//  Fetcher
+//
+//  Fetcher implementation will be added in follow-up changes.
+//
---
+++ Projects/Shared/Fetcher/Tests/Sources/Empty.swift
@@ -0,0 +1,6 @@
+//
+//  Empty.swift
+//  FetcherTests
+//
+//  Fetcher tests will be added with the implementation.
+//
